f970656d276d796e18633b2817bc4c1f889c4902
emman <emmanuel240158@gmail.com>
2026-04-07 21:13:30 -0500

    Refactor:Redesign footer with dynamic categories, enhanced contact details, social media links, and improved styles. Add year binding and animations for interactive elements.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {CategoryService} from '../../services/category/category.service';
+import {CategoryNode} from '../../models/category';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.sass'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+
+  year = new Date().getFullYear();
+
+  private readonly categoryService = inject(CategoryService);
+  readonly categories = signal<CategoryNode[]>([]);
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(nodes => {
+      this.categories.set(nodes);
+    });
+  }
 
 }
