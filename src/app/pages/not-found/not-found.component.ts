1584ee5b45de796b1719c0b2c1d63b850d8c6b6a
emman <emmanuel240158@gmail.com>
2026-04-08 22:01:29 -0500

    style: Update not-found component with new design and animations.

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.sass'
 })
